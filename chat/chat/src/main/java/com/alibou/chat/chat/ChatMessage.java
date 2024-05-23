8146d61987df7c3186166cb960cdb478e4bf571d
j0hun <dudgns3681@gmail.com>
2024-05-23 19:53:33 +0900

    스프링 웹소켓 공부

---
+++ chat/chat/src/main/java/com/alibou/chat/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.alibou.chat.chat;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+
+}
---
+++ chat/chat/src/main/java/com/alibou/chat/chat/MessageType.java
@@ -0,0 +1,9 @@
+package com.alibou.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+
+}
