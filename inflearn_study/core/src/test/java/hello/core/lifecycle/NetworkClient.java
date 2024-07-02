6f2429589f33e10bbe6802808b484e383ec78aee
j0hun <dudgns3681@gmail.com>
2024-07-02 15:35:06 +0900

    스프링 핵심 원리 기본편 공부

---
+++ inflearn_study/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,34 @@
+package hello.core.lifecycle;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @PostConstruct
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    @PreDestroy
+    public void disconnect(){
+        System.out.println("close: " + url);
+    }
+
+}
