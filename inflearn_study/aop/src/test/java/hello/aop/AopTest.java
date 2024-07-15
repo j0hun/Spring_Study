4ac332b0a631cc61f4bd1dd5c02087bbf3f62d6c
j0hun <dudgns3681@gmail.com>
2024-07-15 16:47:21 +0900

    스프링 핵심 원리 고급편 공부

---
+++ inflearn_study/aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,43 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Service;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@Import(AspectV6Service.class)
+@SpringBootTest
+public class AopTest {
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}",
+                AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}",
+                AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
